6e0651498688a149a5720c9c18bc87abcea0e3df
Alexander <aleksievisa@gmail.com>
2023-07-09 16:24:05 +0300

    feat: add models

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -5,4 +5,12 @@ export class ProductModel {
   oldPrice: number;
   credit: number;
   calculatedRating: number;
+  description: string;
+  advantages: string;
+  disAdvantages: string;
+  categories: string[];
+  tags: string;
+  characteristics: {
+    [key: string]: string;
+  };
 }
