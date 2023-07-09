9b236b64837a2e1d43cd41283922627f5f4532f2
Alexander <aleksievisa@gmail.com>
2023-07-10 00:12:18 +0300

    feat: add validation pipe

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -13,6 +13,7 @@ import {
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
 
 @Controller('review')
 export class ReviewController {
@@ -25,7 +26,7 @@ export class ReviewController {
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {
+  async delete(@Param('id', IdValidationPipe) id: string) {
     const deletedDoc = await this.reviewService.delete(id);
     if (!deletedDoc) {
       throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
@@ -33,7 +34,7 @@ export class ReviewController {
   }
 
   @Get('byProduct/:productId')
-  async get(@Param('productId') productId: string) {
+  async get(@Param('productId', IdValidationPipe) productId: string) {
     return this.reviewService.findByProductId(productId);
   }
 }
